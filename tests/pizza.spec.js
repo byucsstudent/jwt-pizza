6b607ab4765cb7fa53d4dbabc06c87f15433f158
Lee <leesjensen@gmail.com>
2024-05-25 15:39:07 -0600

    fix(test) was still calling service instead of mock for menu

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from 'playwright-test-coverage';
 
 test('purchase with login', async ({ page }) => {
-  await page.route('*/**/api/menu', async (route) => {
+  await page.route('*/**/api/order/menu', async (route) => {
     const menuRes = [
       { id: 1, title: 'Veggie', image: 'pizza1.png', price: 0.0038, description: 'A garden of delight' },
       { id: 2, title: 'Pepperoni', image: 'pizza2.png', price: 0.0042, description: 'Spicy treat' },
@@ -70,7 +70,7 @@ test('purchase with login', async ({ page }) => {
   // Create order
   await expect(page.locator('h2')).toContainText('Awesome is a click away');
   await page.getByRole('combobox').selectOption('1');
-  await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+  await page.getByRole('link', { name: 'Image Description Veggie' }).click();
   await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
   await expect(page.locator('form')).toContainText('Selected pizzas: 2');
   await page.getByRole('button', { name: 'Checkout' }).click();
