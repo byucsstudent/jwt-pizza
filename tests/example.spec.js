4df04e780b5c37af74764860e1f58790f7080bdf
Lee <leesjensen@gmail.com>
2024-10-03 16:05:12 -0600

    add(test) login 37.97%

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,3 +5,16 @@ test('home page', async ({ page }) => {
 
   expect(await page.title()).toBe('JWT Pizza');
 });
+
+test('login', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByPlaceholder('Email address').click();
+  await page.getByPlaceholder('Email address').fill('demo@jwt.com');
+  await page.getByPlaceholder('Password').fill('d');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'd', exact: true })).toBeVisible();
+  await page.getByRole('link', { name: 'd', exact: true }).click();
+  await expect(page.getByText('demo@jwt.com')).toBeVisible();
+});
