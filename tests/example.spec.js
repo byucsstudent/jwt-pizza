f6707fc3f35fe9b7798bdc9b92f27564201b3fe4
Lee <leesjensen@gmail.com>
2024-10-03 16:26:28 -0600

    add(test) mock login

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,14 +7,22 @@ test('home page', async ({ page }) => {
 });
 
 test('login', async ({ page }) => {
+  await page.route('*/**/api/auth', async (route) => {
+    const loginReq = { email: 'z@jwt.com', password: 'a' };
+    const loginRes = { user: { id: 3, name: 'Kai Chen', email: 'z@jwt.com', roles: [{ role: 'diner' }] }, token: 'abcdef' };
+    expect(route.request().method()).toBe('PUT');
+    expect(route.request().postDataJSON()).toMatchObject(loginReq);
+    await route.fulfill({ json: loginRes });
+  });
+
   await page.goto('/');
 
   await page.getByRole('link', { name: 'Login' }).click();
   await page.getByPlaceholder('Email address').click();
-  await page.getByPlaceholder('Email address').fill('demo@jwt.com');
-  await page.getByPlaceholder('Password').fill('d');
+  await page.getByPlaceholder('Email address').fill('z@jwt.com');
+  await page.getByPlaceholder('Password').fill('a');
   await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page.getByRole('link', { name: 'd', exact: true })).toBeVisible();
-  await page.getByRole('link', { name: 'd', exact: true }).click();
-  await expect(page.getByText('demo@jwt.com')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'KC', exact: true })).toBeVisible();
+  await page.getByRole('link', { name: 'KC', exact: true }).click();
+  await expect(page.getByText('z@jwt.com')).toBeVisible();
 });
