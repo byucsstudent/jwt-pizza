fa0e54a9968ecf0c5ebc8bd72c649c082c166194
Lee <leesjensen@gmail.com>
2025-01-29 14:04:37 -0700

    impl(test) simple login 19.36%

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from 'playwright-test-coverage';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
+  expect(await page.title()).toBe('JWT Pizza');
 });
